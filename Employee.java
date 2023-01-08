1e80e99a2066231f035f6820fb67a7971fd7b82e
HadiftaAttallah <114393928+HadiftaAttallah@users.noreply.github.com>
2023-01-08 14:10:43 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HadiftaAttallahPutri_2101092049_modul11;
+
+/**
+ *
+ * @author user
+ */
+public class Employee extends Person {
+    
+    @Override
+    public String getName(){ 
+        System.out.println("getName Employee");
+        return name; 
+    } 
+}
